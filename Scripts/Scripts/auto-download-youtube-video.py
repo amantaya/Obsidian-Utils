8ae71e96e9e050364eb62ae113522f044bbb73f8
amantaya <aantaya@email.arizona.edu>
2023-09-30 14:17:32 -0700

    vault backup: 2023-09-30 14:17:32
    Affected files:
    .obsidian/core-plugins.json
    .obsidian/workspace.json
    Scripts/auto-download-youtube-video.py

--- Scripts/Scripts/auto-download-youtube-video.py
+++ Scripts/Scripts/auto-download-youtube-video.py
@@ -2,27 +2,26 @@ import os
 from os import path
 from pathlib import Path
 from time import sleep
-
 import frontmatter
 
 # set the working to directory to the project root
 os.chdir("../")
 
 # read in the YAML frontmatter from the first file in "Inbox" folder
-with open("Inbox/2023-09-25-20-04-26,md", "r") as file:
+with open("Inbox/2023-09-25-20-04-26.md", "r", encoding='utf8') as file:
     post = frontmatter.load(file)
+    lines = file.readlines()
+
 
 # get the URL from the YAML frontmatter
 url = post["URL"]
 
 # detect if the URL is a YouTube video
 if "youtube.com" in url:
-    # extract the video ID from the URL
-    video_id = url.split("v=")[1]
 
     # construct the command to download the video
     # TODO what current directory is the command being executed in?
-    command = f"youtube-dl.exe -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' -o 'Inbox/%(title)s.%(ext)s' https://www.youtube.com/watch?v={video_id}"
+    command = f"youtube-dl.exe -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' -o 'Inbox/%(title)s.%(ext)s' {url}"
 
     # execute the command
     os.system(command)
@@ -33,11 +32,12 @@ if "youtube.com" in url:
     # get the filename of the downloaded video
     video_filename = Path("Inbox").glob("*.mp4")[0]
 
-    # set the title of the post to the title of the video
-    post["title"] = post["title"] + video_filename
+    # write the video filename to the H1 in the Markdown file
+    for line in lines:
+        if line.startswith('# '):
+            line = line + video_filename
+            print(line)
 
-    # write the YAML frontmatter back to the file
-    frontmatter.dump(post, file)
 
 # TODO Option 2 - write the video title to H1 in the Markdown file
 
